2f1fc174b5f262d7a8701ab4d1351eb7317b3c7a
Rasmus <rasmustaah@gmail.com>
2024-02-16 10:26:56 +0100

    track correct files

---
+++ src/nizk/nizkpok_dl.hpp
@@ -0,0 +1,30 @@
+#ifndef NIZKPOK_DL_HPP__
+#define NIZKPOK_DL_HPP__
+
+#include "qclpvss_utils.hpp"
+#include "bicycl.hpp"
+
+namespace NIZKPOK_DL_
+{
+    using namespace UTILS;
+    using namespace BICYCL;
+    using namespace OpenSSL;
+
+    class NizkPoK_DL {
+
+        protected:
+        Mpz A_;
+        HashAlgo & h_;
+        std::vector<Mpz> b_, u_;
+
+        public:
+
+        NizkPoK_DL(HashAlgo &hash, RandGen &randgen, const CL_HSMqk &cl_hsm,
+            const SecLevel & seclevel, const PublicKey &x, const SecretKey &w);
+
+        bool Verify(const CL_HSMqk &cl_hsm, const PublicKey &x) const;
+    };
+
+}
+
+#endif
